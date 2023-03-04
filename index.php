16436cb427ed02553a9b718d7f7594f945620f69
Armando Salazar <armandosalazar351@gmail.com>
2023-03-03 21:51:18 -0600

    add image to post

--- index.php
+++ index.php
@@ -19,7 +19,28 @@ if (isset($_SESSION["session"])) {
 </head>
 
 <body>
+    <div>
+        <h2>New post</h2>
+        <form action="/" method="post">
+            <input type="file" name="image" accept="image/*">
+            <input type="text" name="post" require>
+            <input type="submit">
+        </form>
+    </div>
     <button onclick="window.location='/login.php'">Login</button>
 </body>
 
 </html>
+
+<?php
+if (isset($_POST["post"]) && !empty($_POST["post"])) {
+    $post = $_POST["post"];
+    var_dump($_POST);
+    var_dump($_FILES);
+    $target_dir="uploads/";
+    $target_file = $target_dir . basename($_FILES['image']);
+    if (getimagesize($_FILES["image"]["tmp_name"]))
+        echo "PPP";
+    echo "<h2>$post</h2>";
+}
+?>
